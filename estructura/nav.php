954f1bba8e6074cf909963b2af48b7793c0edb0a
Jhonn Leal <44322151+jhonnjairoleal@users.noreply.github.com>
2024-09-14 10:54:44 -0500

    actualizado con base login

--- estructura/nav.php
+++ estructura/nav.php
@@ -1,11 +1,40 @@
-<ul class="nav justify-content-center">
-  <li>
-    
-  </li>
-  <li class="nav-item">
-    <a class="nav-link active" href="../home/">Inicio</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="../../">Salir</a>
-  </li>
-</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Tecnology Solutions</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto"> <!-- Cambiado mr-auto a ml-auto para alinear a la derecha -->
+      <li class="nav-item active">
+        <a class="nav-link" href="../../vistas/admin/">Servicios <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Marcas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Acerca de</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../../vistas/login.html">Ingreso</a>
+      </li>
+<!--       <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+          Contacto
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Acerca de</a>
+          <a class="dropdown-item" href="#">Ingreso</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> -->
+<!--       <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li> -->
+    </ul>
+  </div>
+</nav>
--- vistas/Home/index.php
+++ vistas/Home/index.php
@@ -3,10 +3,36 @@ require '../../estructura/header.php';
 include '../../estructura/nav.php';
 ?>
 
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </button>
+</div>
 
 
 
 <?php 
 require '../../estructura/footer.php';
 ?>
-<script type="text/javascript" src="../../public/js/usuarios.js"></script>
+<script type="text/javascript" src="../../public/js/home.js"></script>
