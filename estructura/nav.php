935a1f95399cd6a090a877cfb28f7196fc9d73fa
Jhonn Leal <44322151+jhonnjairoleal@users.noreply.github.com>
2024-09-14 09:50:50 -0500

    Estructura base - bootstrap, jq, js. home/index

---
+++ estructura/nav.php
@@ -0,0 +1,11 @@
+<ul class="nav justify-content-center">
+  <li>
+    
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active" href="../home/">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="../../">Salir</a>
+  </li>
+</ul>
---
+++ vistas/Home/index.php
@@ -0,0 +1,12 @@
+<?php 
+require '../../estructura/header.php';
+include '../../estructura/nav.php';
+?>
+
+
+
+
+<?php 
+require '../../estructura/footer.php';
+?>
+<script type="text/javascript" src="../../public/js/usuarios.js"></script>
