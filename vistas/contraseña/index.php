52651ddc865ddac71018bbf474a1e5191500939b
srmmc200031 <97027030+srmmc200031@users.noreply.github.com>
2024-09-23 18:07:17 +0000

    Se trabajó en registro y contraseña, creando esta ultima como carpeta aldeaña.

--- vistas/contraseña/index.php
+++ vistas/contraseña/index.php
@@ -7,7 +7,7 @@ include '../../estructura/nav.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Registro</title>
+	<title>Reestablecer Contraseña</title>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
@@ -18,19 +18,11 @@ include '../../estructura/nav.php';
 <body>
 	<center>
 		<form>
-			<label>Registro</label><br>
+			<label>Reestablecer Contraseña</label><br>
 			<hr>
-			<label>Nombre</label>
-			<input type="" name=""><br>
-			<label>Apellido</label>
-			<input type="" name=""><br>
 			<label>Correo</label>
 			<input type="" name=""><br>
-			<label>Contraseña</label>
-			<input type="" name=""><br>
-			<button class="btn btn-primary btn-lg" type="button" onclick="login()">Crear cuenta</button><br>
-			<button class="btn btn-secondary btn-lg" type="button" onclick="window.location.href='/vistas/contraseña/index.php'">Olvidé mi contraseña</button>
-
+			<button class="btn btn-primary btn-lg" type="button" onclick="login()">Reestablecer</button><br>
 		</form>
 	</center>
 </body>
